3cddb6c6cfd77b21eb248af3f48e9fc46023ec79
Laura Kopf <laura.kopf@gmx.de>
2022-10-28 10:06:27 +0000

    Added vico repo

---
+++ evaluation/vico_eval/data/semeval_2018_10/preprocess.sh
@@ -0,0 +1,4 @@
+python -m evaluation.vico.data.semeval_2018_10.convert_to_json
+python -m evaluation.vico.data.semeval_2018_10.compute_word_freq
+python -m evaluation.vico.data.semeval_2018_10.compute_feature_freq
+python -m evaluation.vico.data.semeval_2018_10.merge_word_feature_freq
